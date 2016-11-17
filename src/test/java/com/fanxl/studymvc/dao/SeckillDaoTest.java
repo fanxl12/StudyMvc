8b7ebb0405d1c6dc1c5f5d3d9a5845ab2e4978e2
fanxl2 <fanxl2@centaline.com.cn>
2016-11-17 17:09:13 +0800

    1.添加测试； 2.解决几个bug;

---
+++ src/test/java/com/fanxl/studymvc/dao/SeckillDaoTest.java
@@ -0,0 +1,41 @@
+package com.fanxl.studymvc.dao;
+
+import com.fanxl.studymvc.entity.Seckill;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by fanxl2 on 2016/11/17.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SeckillDaoTest {
+
+    //注入Dao实现类依赖
+    @Resource
+    private SeckillDao seckillDao;
+
+    @Test
+    public void queryById() throws Exception {
+        long id = 1000;
+        Seckill seckill = seckillDao.queryById(id);
+        System.out.println(seckill.getName());
+        System.out.println(seckill);
+    }
+
+    @Test
+    public void queryAll() throws Exception {
+
+    }
+
+    @Test
+    public void reduceNumber() throws Exception {
+
+    }
+}
