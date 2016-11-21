6161f4166ace7289bcd8e0e8082059e36feec149
fanxl2 <fanxl2@centaline.com.cn>
2016-11-21 17:18:56 +0800

    添加Controller；

---
+++ src/main/java/com/fanxl/studymvc/web/SeckillController.java
@@ -0,0 +1,46 @@
+package com.fanxl.studymvc.web;
+
+import com.fanxl.studymvc.entity.Seckill;
+import com.fanxl.studymvc.service.SeckillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * Created by fanxl2 on 2016/11/21.
+ */
+@Controller
+@RequestMapping("/seckill")
+public class SeckillController {
+
+    @Autowired
+    private SeckillService seckillService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model){
+
+        List<Seckill> list = seckillService.getSeckillList();
+        model.addAttribute("list", list);
+
+        return "list";
+    }
+
+    @RequestMapping(value = "/{seckillId}/detail", method = RequestMethod.GET)
+    public String detail(@PathVariable("seckillId") Long seckillId, Model model){
+        if (seckillId==null){
+            return "redirect:/seckill/list";
+        }
+        Seckill seckill = seckillService.getById(seckillId);
+        if (seckill==null){
+            return "forward:/seckill/list";
+        }
+        model.addAttribute("seckill", seckill);
+        return "detail";
+    }
+
+}
